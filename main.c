302fde9c02a2b3459e84e88b4b111bb1993f86cb
rptaszek <rptaszek.gmail.com>
2018-10-26 10:27:08 +0200

    zastosowanie pętli if poprawiony wyświetla wszystkie

--- main.c
+++ main.c
@@ -2,14 +2,20 @@
 
 
 int main() {
-    printf("największa liczba niepodzielna przez 2, 3, 5, 7.");
+    printf("liczby niepodzielne przez 2, 3, 5, 7.\n");
     int a = 0;
     printf("Podaj a: ");
     scanf("%d", &a);
     for (int liczba = a; liczba >= 1; --liczba) {
-        if (liczba % 2 && liczba % 3 && liczba % 5 && liczba % 7) {
-            printf("%d\n", liczba);
-            break;
+        if (liczba % 2) {
+            if (liczba % 3) {
+                if (liczba % 5) {
+                    if (liczba % 7) {
+                        printf("%d\t", liczba);
+
+                    }
+                }
+            }
         }
     }
     return 0;
